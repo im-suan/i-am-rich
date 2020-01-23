17083d2800a7f0420b6dec03479d6e91a836da34
suan <teko@Stupid-Book.local>
2020-01-23 18:06:07 +0700

    init project

---
+++ i-am-rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i-am-rich
+//
+//  Created by Nguyen Xuan on 1/23/20.
+//  Copyright © 2020 Nguyen Xuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
