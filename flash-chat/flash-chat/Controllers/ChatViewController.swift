20fb257a0c8b3ae0ef3a02f012105c2a74950600
suan <teko@Stupid-Book.local>
2020-01-27 20:24:07 +0700

    show another's avatar and messages, auto scroll to the last message

--- flash-chat/flash-chat/Controllers/ChatViewController.swift
+++ flash-chat/flash-chat/Controllers/ChatViewController.swift
@@ -49,6 +49,8 @@ class ChatViewController: UIViewController {
                             
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: false)
                             }
                         }
                     }
@@ -94,8 +96,24 @@ extension ChatViewController:UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label?.text = messages[indexPath.row].body
+        cell.label?.text = message.body
+        
+        //This is a message from the current user
+        if message.sender == Auth.auth().currentUser?.email {
+            cell.rightImageView.isHidden = false
+            cell.letImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }
+        //This is a message from another user
+        else {
+            cell.rightImageView.isHidden = true
+            cell.letImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
         return cell
     }
 }
